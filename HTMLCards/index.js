4f3cd098fb50bc0df5fe0b519b335c4e0a719b33
Janosch <github@janosch.com>
2021-06-13 20:57:47 +0200

    SneakerCard Final
    even more JS added

--- HTMLCards/index.js
+++ HTMLCards/index.js
@@ -1,52 +1,84 @@
-console.log("Hallo Sneaker");
-let karte = document.querySelector(".karte")
+console.log("Welcome to SneakerWorld");
 
-karte.addEventListener("mouseenter", e => {
-    document.querySelector(".karte").style.gridTemplateColumns = "800px";
-})
-karte.addEventListener("mouseleave", e => {
-    document.querySelector(".karte").style.gridTemplateColumns = "430px";
-    document.querySelector(".karte").style.gridTemplateRows = "260px 288px 2px 115px";
-    document.querySelector(".titel").style.fontSize = "34px";
-    document.querySelector(".typ").style.fontSize = "18px";
-    document.querySelector(".text").style.margin = "35px";
-})
+let karte = document.querySelector(".karte");
 
-let bild = document.querySelector(".bild")
+karte.addEventListener("mouseenter", (e) => {
+  document.querySelector(".karte").style.gridTemplateColumns = "800px";
+});
+karte.addEventListener("mouseleave", (e) => {
+  document.querySelector(".karte").style.gridTemplateColumns = "430px";
+  document.querySelector(".karte").style.gridTemplateRows =
+    "260px 288px 2px 115px";
+  document.querySelector(".titel").style.fontSize = "34px";
+  document.querySelector(".typ").style.fontSize = "18px";
+  document.querySelector(".text").style.margin = "35px";
+});
 
-bild.addEventListener("mouseenter", e => {
-    document.querySelector(".karte").style.gridTemplateRows = "360px 188px 2px 115px";
-    document.querySelector(".text").style.margin = "10px";
-})
+let bild = document.querySelector(".bild");
 
-bild.addEventListener("mouseleave", e => {
-    document.querySelector(".karte").style.gridTemplateRows = "260px 288px 2px 115px";
-    document.querySelector(".text").style.margin = "35px";
-})
+bild.addEventListener("mouseenter", (e) => {
+  document.querySelector(".karte").style.gridTemplateRows =
+    "360px 188px 2px 115px";
+  document.querySelector(".text").style.margin = "10px";
+});
 
-let titel = document.querySelector(".titel")
+bild.addEventListener("mouseleave", (e) => {
+  document.querySelector(".karte").style.gridTemplateRows =
+    "260px 288px 2px 115px";
+  document.querySelector(".text").style.margin = "35px";
+});
 
-titel.addEventListener('mouseenter', e => {
-    document.querySelector(".karte").style.gridTemplateRows = "230px 318px 2px 115px";
-    document.querySelector(".titel").style.fontSize = "95px";
-    document.querySelector(".typ").style.fontSize = "35px";
-    document.querySelector(".text").style.margin = "10px";
-    document.querySelector(".titel").style.margin = "10px";
-})
+let titel = document.querySelector(".titel");
 
-titel.addEventListener('mouseleave', e => {
-    document.querySelector(".karte").style.gridTemplateRows = "260px 288px 2px 115px";
-    document.querySelector(".titel").style.fontSize = "34px";
-    document.querySelector(".typ").style.fontSize = "18px";
-    document.querySelector(".text").style.margin = "35px";
-})
+titel.addEventListener("mouseenter", (e) => {
+  document.querySelector(".karte").style.gridTemplateRows =
+    "230px 318px 2px 115px";
+  document.querySelector(".titel").style.fontSize = "95px";
+  document.querySelector(".typ").style.fontSize = "35px";
+  document.querySelector(".text").style.margin = "10px";
+  document.querySelector(".titel").style.margin = "10px";
+});
 
-let kurztext = document.querySelector(".kurztext")
+titel.addEventListener("mouseleave", (e) => {
+  document.querySelector(".karte").style.gridTemplateRows =
+    "260px 288px 2px 115px";
+  document.querySelector(".titel").style.fontSize = "34px";
+  document.querySelector(".typ").style.fontSize = "18px";
+  document.querySelector(".text").style.margin = "35px";
+});
 
-kurztext.addEventListener('mouseenter', e => {
-    document.querySelector(".kurztext").style.fontSize = "28px";
-})
+let kurztext = document.querySelector(".kurztext");
 
-kurztext.addEventListener('mouseleave', e => {
-    document.querySelector(".kurztext").style.fontSize = "20px";
-})
+kurztext.addEventListener("mouseenter", (e) => {
+  document.querySelector(".kurztext").style.fontSize = "28px";
+});
+
+kurztext.addEventListener("mouseleave", (e) => {
+  document.querySelector(".kurztext").style.fontSize = "20px";
+});
+
+// ab jetzt mit Tutorial https://www.youtube.com/watch?v=a6XIMIKmj9k
+
+const counters = document.querySelectorAll(".counter");
+const speed = 3000; // Je höher desto langsamer
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    // Geschwindigkeitsumrechnung
+    const inc = target / speed;
+
+    // Überprüft ob max erreicht
+    if (count < target) {
+      counter.innerText = count + inc;
+
+      setTimeout(updateCount, 300);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
